90d3412f480f33355a33acec2a1fe754391be954
Olivia <oliviapower.02@gmail.com>
2020-01-15 16:56:20 +0000

    added server to play.js

--- play.js
+++ play.js
@@ -8,4 +8,26 @@ const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
-game.start()
+game.start();
+
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
