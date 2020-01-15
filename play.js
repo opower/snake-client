f781b6beeb943f922ddef8da853eb5f1506580a1
Olivia <oliviapower.02@gmail.com>
2020-01-15 21:14:55 +0000

    stdin input functionality

--- play.js
+++ play.js
@@ -7,9 +7,29 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", (key)=>{
+    if(key === '\u0003'){
+      process.exit();
+    }
+  });
+
+  return stdin;
+}
+
+
+
 // Begin game
 game.start();
 
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+setupInput();
+
+
